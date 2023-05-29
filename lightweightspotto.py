41004727d91b0d6c69202a20cbdfc0f05d314193
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-05-29 21:36:27 +1000

    Update lightweightspotto.py

--- lightweightspotto.py
+++ lightweightspotto.py
@@ -2,6 +2,6 @@ try:
   import os, webview
   from tkinter import messagebox
 except: return print("Nessacery packages aren't installed. Please run the install script again.")
-if messagebox.askokcancel("
-                      "Want to launch Spotify? Lightweight Spotify may take up a few bytes of your disk.")
-webview.create_window("", "").start()
+if messagebox.askokcancel("Spotify",
+                      "Want to launch Spotify? Lightweight Spotify may take up a few bytes of your disk."):
+  webview.create_window("Spotify", "https://open.spotify.com/__noul__").start()
