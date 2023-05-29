3e0959ef8a54dba3d96a3b63bd4c2ff393c39203
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-05-29 21:53:15 +1000

    Update lightweightspotto.py

--- lightweightspotto.py
+++ lightweightspotto.py
@@ -5,4 +5,5 @@ try:
 except: print("Nessacery packages aren't installed. Please run the install script again."); sys.exit(1)
 if messagebox.askokcancel("Spotify",
                       "Want to launch Spotify? Lightweight Spotify may take up a few bytes of your disk."):
-  webview.create_window("Spotify", "https://open.spotify.com/__noul__").start()
+  webview.create_window("Spotify", "https://open.spotify.com/__noul__")
+  webview.start()
